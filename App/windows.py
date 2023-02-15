f7bca988c60c9b7c345510747cd15f1e6cdc1f36
yoseidonn <yk.yusuf.2006@gmail.com>
2023-02-15 12:19:49 +0300

    Icon değişimi

--- App/windows.py
+++ App/windows.py
@@ -47,7 +47,7 @@ class MainWindow(QMainWindow):
         actNewExam.triggered.connect(self.yeni_sinav_frame)
         self.toolBar.addAction(actNewExam)
         # ÇIKTILAR
-        actShowExams = QAction(QIcon(os.path.join("Images", "icon", "list.svg")), "Sınavları göster", self)
+        actShowExams = QAction(QIcon(os.path.join("Images", "img", "files.png")), "Sınavları göster", self)
         actShowExams.triggered.connect(self.sinavlar_frame)
         self.toolBar.addAction(actShowExams)
         # GOZETMEN
@@ -687,13 +687,10 @@ class SalonlarFrame(QFrame):
         self.addButton.setVisible(False)
 
         name, teacherd, hspd, layout = values
-        print(values)
         self.salonNameIn.setText(name)
         self.Classroom.set_layout(layout)
         self.yonCombo.setCurrentText(teacherd)
         self.kacliCombo.setCurrentText(hspd)
-        print("drawed")
-        print()
 
     def draw_list(self):
         self.classroomList.clear()
